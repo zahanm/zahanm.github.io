66a6865c496d6a6bc121cb5f6d18245b036e2787
Zahan Malkani <zahanm@gmail.com>
2012-04-07 17:10:12 -0700

    js debug test

---
+++ static/scripts/index.js
@@ -0,0 +1,11 @@
+
+(function(global) {
+  var zz, dReadyListeners, document;
+
+  zz = global.zz;
+  document = global.document;
+
+  console.info('Debug test zzquery:');
+  console.info(zz('section'));
+
+})(window || global);
