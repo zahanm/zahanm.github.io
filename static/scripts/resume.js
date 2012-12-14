f114a1078e931399e1505fd6add784a77d4807ce
Zahan Malkani <zahanm@gmail.com>
2012-12-13 22:13:20 -0800

    [resume2] work experience

--- static/scripts/resume.js
+++ static/scripts/resume.js
@@ -1,12 +1,19 @@
 
 (function (global) {
 
-  zz.json("/api/education.json", function (err, edu) {
-    if (err) {
-      console.error("Call to /api/education.json failed"); return;
-    }
-    var destination = document.querySelector("#education");
-    destination.innerHTML = Mustache.render(destination.innerHTML, edu);
+  fetches = [
+    { loc: "/api/education.json", dest: "#education" },
+    { loc: "/api/workexp.json", dest: "#workexp" }
+  ];
+
+  fetches.forEach(function (f) {
+    zz.json(f.loc, function (err, data) {
+      if (err) {
+        console.error("Call to " + f.loc + " failed"); return;
+      }
+      var destination = document.querySelector(f.dest);
+      destination.innerHTML = Mustache.render(destination.innerHTML, data);
+    });
   });
 
 }(window));
