5ccd03ae8f63963946a1233599a7b0ee790d6a18
Zahan Malkani <zahanm@gmail.com>
2015-06-28 00:10:17 -0700

    this is finally outputting markdown in a template

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,16 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   less = require('gulp-less'),
-  autoprefix = new (require('less-plugin-autoprefix'))();
+  autoprefix = new (require('less-plugin-autoprefix'))()
+  markdown = require('gulp-markdown'),
+  wrap = require('gulp-wrap'),
+  tap = require('gulp-tap'),
+  path = require('path'),
+  assert = require('assert'),
+  template = require('gulp-template'),
+  data = require('gulp-data'),
+  log = require('gulp-util').log,
+  debug = require('gulp-debug');
 
 gulp.task('server', function() {
   connect.server({
@@ -16,6 +25,23 @@ gulp.task('less', function() {
       plugins: [autoprefix]
     }))
     .pipe(gulp.dest('css/'));
+});
+
+gulp.task('markdown', function () {
+  return gulp.src('markdown/*.md')
+    .pipe(markdown())
+    .pipe(debug({ title: 'pre' }))
+    .pipe(tap(function (file, t) {
+      var name = path.basename(file.path).split('.')[0];
+      return gulp.src('templates/' + name + '.jst')
+        .pipe(debug({ title: 'in' }))
+        .pipe(data(function () {
+          assert(file.isBuffer(), 'cannot do streams atm');
+          return { contents: file.contents };
+        }))
+        .pipe(template())
+        .pipe(gulp.dest('temp/'));
+    }));
 });
 
 gulp.task('watch', function () {
