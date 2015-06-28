a68d17e9198894ca42670bc5d60dfa5636835495
Zahan Malkani <zahanm@gmail.com>
2015-06-27 21:05:51 -0700

    less task
    adds a `base.css`, but that's okay because i'm not listing out things by
    hand any more

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 
 var gulp = require('gulp'),
-  connect = require('gulp-connect');
+  connect = require('gulp-connect'),
+  less = require('gulp-less'),
+  autoprefix = new (require('less-plugin-autoprefix'))();
 
 gulp.task('server', function() {
   connect.server({
     port: 8000
   });
+});
+
+gulp.task('less', function() {
+  return gulp.src('less/*.less')
+    .pipe(less({
+      plugins: [autoprefix]
+    }))
+    .pipe(gulp.dest('css'))
 });
